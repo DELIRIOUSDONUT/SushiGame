6a864ff4aeef999c69ee0ed20d3a313a7636476d
KEN_LIM456 <31853051+DELIRIOUSDONUT@users.noreply.github.com>
2025-07-09 14:29:38 +0700

    ios build

---
+++ Assets/Scripts/Prefs.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Prefs : MonoBehaviour
+{
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
